3109d117f32a14ac2c06f69db9a4c2687fedb9aa
Jonas <neo.stief.jon@gmail.com>
2026-06-07 16:00:25 +0200

    Take the calander and paste it into the Dashboard_calander.vue

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,37 +1,18 @@
 <script setup lang="ts">
-import VueCal from "vue-cal";
-import "vue-cal/dist/vuecal.css";
-
-const handleCellClick = (date: any) => {
-  alert(`Du hast auf den Tag geklickt: ${date.toLocaleDateString("de-DE")}`);
-};
+import Dashboard_calander from "./Dashboard_calander.vue";
 </script>
 
 <template>
-  <h1>Mein Dashboard</h1>
-  <vue-cal
-    active-view="week"
-    :disable-views="['years', 'year', 'month', 'day']"
-    :locale="'de'"
-    :time="false"
-    @cell-click="handleCellClick"
-    class="vuecal--square dashboard-cal"
-  />
+    <h1>Mein Dashboard</h1>
+    <dashboard_calander />
 </template>
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.dashboard-cal {
-  height: 250px;
-  border: 1px solid #333;
-  border-radius: 8px;
-  overflow: hidden;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/Dashboard/Dashboard_calander.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+const handleCellClick = (date: any) => {
+    alert(`Du hast auf den Tag geklickt: ${date.toLocaleDateString("de-DE")}`);
+};
+</script>
+
+<template>
+    <vue-cal
+        active-view="week"
+        :disable-views="['years', 'year', 'month', 'day']"
+        :locale="'de'"
+        :time="false"
+        @cell-click="handleCellClick"
+        class="vuecal--square dashboard-cal"
+    />
+</template>
+
+<style lang="css" scoped>
+.dashboard-cal {
+    height: 250px;
+    border: 1px solid #333;
+    border-radius: 8px;
+    overflow: hidden;
+}
+</style>
