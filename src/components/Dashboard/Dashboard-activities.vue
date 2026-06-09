59eec0e29653e709e80bd1b1f35a7bf4405b2c23
Jonas <neo.stief.jon@gmail.com>
2026-06-09 22:14:09 +0200

    heatmap changes and add a prototype timer

--- src/components/Dashboard/Dashboard-activities.vue
+++ src/components/Dashboard/Dashboard-activities.vue
@@ -3,22 +3,24 @@ import { ref } from "vue";
 import { CalendarHeatmap } from "vue3-calendar-heatmap";
 import "vue3-calendar-heatmap/dist/style.css";
 
+const today = new Date();
+const todayFormatted = today.toISOString().split("T")[0];
+const customColors = ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"];
+
 const pomodoroHistory = ref([
     { date: "2026-06-01", count: 2 },
     { date: "2026-06-02", count: 5 },
     { date: "2026-06-03", count: 0 },
     { date: "2026-06-04", count: 12 },
 ]);
-const customColors = ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"];
 </script>
 
 <template>
     <calendar-heatmap
         :values="pomodoroHistory"
-        :end-date="'2026-12-31'"
+        :end-date="todayFormatted"
         :round="2"
         :range-color="customColors"
-        :max="10"
     />
 </template>
 
--- src/components/Timer/Timer.vue
+++ src/components/Timer/Timer.vue
@@ -1,8 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStopwatch } from "vue-timer-hook";
+
+const autoStart = false;
+const stopwatch = useStopwatch(0, autoStart);
+</script>
 
 <template>
-  <h1>Timer</h1>
-  <div>Hier kommt der Timer hin.</div>
+    <div class="timer">
+        <h1>Timer</h1>
+        <p>Stopwatch Demo</p>
+        <div>
+            <span>{{ stopwatch.days }}</span> :
+            <span>{{ stopwatch.hours }}</span> :
+            <span>{{ stopwatch.minutes }}</span> :
+            <span>{{ stopwatch.seconds }}</span>
+        </div>
+        <p>{{ stopwatch.isRunning ? "Running" : "Not running" }}</p>
+        <button @click="stopwatch.start()">Start</button>
+        <button @click="stopwatch.pause()">Pause</button>
+        <button @click="stopwatch.reset()">Reset</button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.timer {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
