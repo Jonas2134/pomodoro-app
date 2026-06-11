b25721393736cbe9bcc49a6dd82623114a5f17a9
Jonas <neo.stief.jon@gmail.com>
2026-06-11 18:30:00 +0200

    add and change timer style

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -4,8 +4,8 @@ import DashboardActivities from "./Dashboard-activities.vue";
 </script>
 
 <template>
-    <div class="dashboard">
-        <h1 class="dashboard__title">Mein Dashboard</h1>
+    <div class="base-layout">
+        <h1 class="headline-center">Mein Dashboard</h1>
         <section class="dashboard__section">
             <dashboard-calander />
             <dashboard-activities />
@@ -14,19 +14,6 @@ import DashboardActivities from "./Dashboard-activities.vue";
 </template>
 
 <style lang="css" scoped>
-.dashboard {
-    max-width: 1200px;
-    padding: 18px;
-    margin: 0 auto;
-    display: flex;
-    gap: 24px;
-    flex-direction: column;
-}
-
-.dashboard__title {
-    text-align: center;
-    text-decoration: underline;
-}
 .dashboard__section {
     display: flex;
     gap: 20px;
--- src/components/Timer/Timer.vue
+++ src/components/Timer/Timer.vue
@@ -6,27 +6,37 @@ const stopwatch = useStopwatch(0, autoStart);
 </script>
 
 <template>
-    <div class="timer">
-        <h1>Timer</h1>
-        <p>Stopwatch Demo</p>
-        <div>
-            <span>{{ stopwatch.days }}</span> :
-            <span>{{ stopwatch.hours }}</span> :
-            <span>{{ stopwatch.minutes }}</span> :
-            <span>{{ stopwatch.seconds }}</span>
+    <div class="base-layout">
+        <h1 class="headline-center">Timer</h1>
+        <div class="timer__section">
+            <p>Stopwatch Demo</p>
+            <div>
+                <span>{{ stopwatch.days }}</span> :
+                <span>{{ stopwatch.hours }}</span> :
+                <span>{{ stopwatch.minutes }}</span> :
+                <span>{{ stopwatch.seconds }}</span>
+            </div>
+            <p>{{ stopwatch.isRunning ? "Running" : "Not running" }}</p>
         </div>
-        <p>{{ stopwatch.isRunning ? "Running" : "Not running" }}</p>
-        <button @click="stopwatch.start()">Start</button>
-        <button @click="stopwatch.pause()">Pause</button>
-        <button @click="stopwatch.reset()">Reset</button>
+        <menu class="timer__menu">
+            <button @click="stopwatch.start()">Start</button>
+            <button @click="stopwatch.pause()">Pause</button>
+            <button @click="stopwatch.reset()">Reset</button>
+        </menu>
     </div>
 </template>
 
 <style scoped>
-.timer {
+.timer__section {
     display: flex;
-    gap: 20px;
+    gap: 10px;
     flex-direction: column;
     align-items: center;
+}
+
+.timer__menu {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
 }
 </style>
